3a1981acd0aeb5370fd83aa29adb08408860c611
NEBULA1978 <rpascualey@gmail.com>
2023-08-09 20:42:21 +0200

    Primera variable con vite para ver el nombre

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// alert("HOla desde js")
+const name = 'vue dinámico'
+</script>
+
+<template>
+<h1>Hola {{ name }}</h1>
+</template>
+
+<style scoped>
+h1{
+  color:red;
+}
+</style>
