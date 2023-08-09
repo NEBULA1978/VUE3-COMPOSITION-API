71f97dd4d0be5a5576dc4b0257ab1454459ee022
NEBULA1978 <rpascualey@gmail.com>
2023-08-09 21:04:59 +0200

    Creo variable para el color con v:bind arrayColores y variable active para condicionales

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,12 +2,22 @@
 // alert("HOla desde js")
 const name = 'vue dinámico'
 const styleColor = "color:blue"
+const arrayColores = ["blue","red","peru"]
+const active = true
+// const active = false
 </script>
 
 <template>
 <h1>Hola {{ name.toLocaleUpperCase() }}</h1>
 
 <h2 v-bind:style="styleColor">Soy AZUL</h2>
+<!-- Cuando cambia el indice cambia el color -->
+<h2 v-bind:style="`color: ${arrayColores[2]}`">Soy Peru </h2>
+
+<h2>
+  {{ active ? "Estoy activo" : "Estoy Inactivo" }}
+</h2>
+<h3>{{ arrayColores }}</h3>
 </template>
 
 <style scoped>
