4b0aba1a9251da155f597cdcc9ef0cda11a92f78
NEBULA1978 <rpascualey@gmail.com>
2023-08-09 23:37:59 +0200

    Creamos contador con ref

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,33 @@
-<script setup>
-const name = 'vue dinámico';
-
-// Métodos
-const handleClick = (message) => {
-  console.log(message);
-};
-
-const arrayFrutas = ['🍏', '🍇', '🍓', '🥝', '🍒'];
-</script>
-
 <template>
   <div class="container">
     <h1>Hola {{ name.toLocaleUpperCase() }}</h1>
-    <!-- Clics del raton -->
-    <button v-on:click.right.prevent="handleClick('Texto Right')">Activame right</button>
-    <button v-on:click="handleClick('Texto Left')">Activame left</button>
-    <button @click.middle="handleClick('Texto Middle')">Activame middle</button>
+    <!-- Botón para incrementar el contador -->
+    <button @click="incrementCounter">Incrementar</button>
+    <!-- Mostrar el valor del contador -->
+    <p>Contador: {{ counter }}</p>
+    <!-- Botón para decrementar el contador -->
+    <button @click="decrementCounter">Decrementar</button>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = 'vue dinámico';
+
+// Crear una variable reactiva para el contador
+const counter = ref(0);
+
+// Función para incrementar el contador
+const incrementCounter = () => {
+  counter.value++;
+};
+
+// Función para decrementar el contador
+const decrementCounter = () => {
+  counter.value--;
+};
+</script>
 
 <style scoped>
 .container {
@@ -27,26 +37,18 @@ const arrayFrutas = ['🍏', '🍇', '🍓', '🥝', '🍒'];
   align-items: center;
   min-height: 100vh;
   padding: 20px;
-  /* Añadido para un espacio alrededor de los elementos */
   text-align: center;
-  /* Centrar el texto en dispositivos móviles */
 }
 
 h1 {
   color: red;
   font-size: 24px;
-  /* Tamaño de fuente más pequeño para dispositivos móviles */
   margin-bottom: 10px;
-  /* Espaciado inferior */
 }
 
 button {
   margin: 5px;
   padding: 10px 20px;
   font-size: 16px;
-  /* Tamaño de fuente más grande para botones en móviles */
-}</style>
-
-<!-- Voy 1h05 del video Fundamentos de Vue 3 con Composition API: Aprende a crear aplicaciones web modernas y escalables
-
-Bluuweb -->
+}
+</style>
