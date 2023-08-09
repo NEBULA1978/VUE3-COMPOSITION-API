3b73329bb4be4a3d8634b085d0dd01aea1f9bc06
NEBULA1978 <rpascualey@gmail.com>
2023-08-09 20:44:59 +0200

    Primera variable con vite para ver el nombre y ver h2 color azaul

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 // alert("HOla desde js")
 const name = 'vue dinámico'
+const styleColor = "color:blue"
 </script>
 
 <template>
 <h1>Hola {{ name }}</h1>
+
+<h2 style="color:blue">Soy AZUL</h2>
 </template>
 
 <style scoped>
