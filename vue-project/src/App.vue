6e0b39ac6dd72c61d6188e48da8b20c3fff0facc
NEBULA1978 <rpascualey@gmail.com>
2023-08-09 23:13:12 +0200

    Eventos de dos botones no dinamico añado css

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,8 +2,8 @@
 const name = 'vue dinámico';
 
 // Métodos
-const handleClick = () => {
-  console.log("Me diste click");
+const handleClick = (message) => {
+  console.log(message);
 };
 
 const arrayFrutas = ['🍏', '🍇', '🍓', '🥝', '🍒'];
@@ -11,12 +11,23 @@ const arrayFrutas = ['🍏', '🍇', '🍓', '🥝', '🍒'];
 
 <template>
   <h1>Hola {{ name.toLocaleUpperCase() }}</h1>
-  <button v-on:click="handleClick">Activame 1</button>
-  <button @click="handleClick">Activame 2</button>
+  <button v-on:click="handleClick('Texto1')">Activame 1</button>
+  <button @click="handleClick('Texto2')">Activame 2</button>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
 h1 {
   color: red;
 }
 </style>
+<!-- Voy 1h05 del video Fundamentos de Vue 3 con Composition API: Aprende a crear aplicaciones web modernas y escalables
+
+Bluuweb -->
