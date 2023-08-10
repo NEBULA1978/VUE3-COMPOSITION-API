2c551ab8de06733fe8ee73a0ed3d2573371f47c8
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 12:11:26 +0200

    Creo componente Hijo2.vue con inyect

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,13 +2,15 @@
 <div>
   <h1>Padre:</h1>
   <HijoVue :nombre="nombre" :edad="edad" @modificar="cambiarValor"/>
+  <HijoVue2 />
 </div>
 
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { provide,ref } from "vue";
 import HijoVue from "./components/Hijo.vue";
+import HijoVue2 from "./components/Hijo2.vue";
 
 const cambiarValor =(valor1,valor2)=>{
   nombre.value=valor1;
@@ -16,6 +18,8 @@ const cambiarValor =(valor1,valor2)=>{
 }
 const nombre = ref('Jab');
 const edad=ref(18);
+const canal=ref("soyjab")
+provide('miCanal',canal)
 </script>
 
 <style scoped>
---
+++ vue-project/src/components/Hijo2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hijo2:</h1>
+        <h2> {{ canal }}</h2>
+        
+    </div>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+
+
+const canal=inject('miCanal')
+</script>
+
+<style scoped></style>
